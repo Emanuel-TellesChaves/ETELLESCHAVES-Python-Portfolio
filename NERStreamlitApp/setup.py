2e12926c458d771ff818d92ccb0beb111dd9e151
Emanuel-TellesChaves <telleschaves738@gmail.com>
2025-04-14 21:50:27 -0400

    Changed python version

--- NERStreamlitApp/setup.py
+++ NERStreamlitApp/setup.py
@@ -4,12 +4,12 @@ setup(
     name="NERStreamlitApp",
     version="0.1.0",
     description="Custom Named Entity Recognition using spaCy and Streamlit",
-    python_requires=">=3.9,<3.10",
+    python_requires=">=3.10,<3.11",
     install_requires=[
-        "streamlit>=1.24.0",
+        "streamlit>=1.32.0",
         "spacy>=3.8.0",
-        "pandas>=1.5.3",
-        "numpy>=1.23.5",
+        "pandas>=2.0.0",
+        "numpy>=1.26.0",
         # Pillow is installed separately in setup.sh
     ],
 ) 
